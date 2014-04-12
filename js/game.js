0fc66c54a3fe3d633cf9f82b26148234e342b71e
Max Zhao <interarticle@gmail.com>
2014-04-12 11:48:13 -0400

    Result page through ng-route

--- js/game.js
+++ js/game.js
@@ -1,10 +1,22 @@
 "use strict";
 
-angular.module("game", []);
-angular.module("game").config(function($compileProvider) {
+angular.module("game", ["ngRoute"]);
+angular.module("game").config(function($compileProvider, $routeProvider) {
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|chrome-extension):/);
     $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|chrome-extension):/);
-}).controller("gameMain", function ($scope) {
+
+    $routeProvider
+    .when ("/", {
+        templateUrl: "pages/index.html",
+        controller: "gameMain"
+    })
+    .when("/result", {
+        templateUrl: "pages/result.html",
+        controller: "resultController"
+    })
+    ;
+})
+.controller("gameMain", function ($scope) {
     $scope.sites = [];
     $scope.ready = false;
     $scope.index = 0;
@@ -12,24 +24,33 @@ angular.module("game").config(function($compileProvider) {
         if(!$scope.ready)
             return;
         $scope.sites[$scope.index++].choice = safe;
-        $('#myRoundabout').roundabout_animateToNextChild();
-        if($scope.index == 10) {
-                        
-        }
 
+        if($scope.index == 10) {
+            location.href = "#/result";
+            return;               
+        }
+        $('#myRoundabout').roundabout_animateToChild($scope.index);
     }
+    $scope.load_progress = "Calculating risk factors...";
     loadData().then(function(data) {
         $scope.$apply(function() {
             $scope.sites = data.result;
 
+            $scope.load_progress = "Preloading thumbnails...";
+
+            var thumbProm = [];
             $.each(data.result, function(index, value) {
-                data.thumbH.getThumbnail(value.url).then(function(url) {
+                var prom = data.thumbH.getThumbnail(value.url).then(function(url) {
                     $scope.$apply(function() {
                         value.thumbnail = url;
                     });
                 });
+                thumbProm.push(prom);
             });
-            $scope.ready = true;
+
+            Promise.all(thumbProm.splice(0, 3)).then(function() {
+                $scope.ready = true;
+            });
             setTimeout(function() {
                 $(document).ready(function () {
                     $('#myRoundabout').roundabout({
@@ -46,4 +67,7 @@ angular.module("game").config(function($compileProvider) {
             }, 0);
         });
     });
+})
+.controller("resultController", function($scope) {
+
 });
