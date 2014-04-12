6c3dfa91f4ba16288d8bd6bf003ebd62781a8664
Elliott <elliott8533@gmail.com>
2014-04-12 07:44:38 -0400

    permission

--- js/game.js
+++ js/game.js
@@ -12,3 +12,40 @@ $(document).ready(function () {
         reflect: true
     });
 });
+
+function loadData() {
+    var inst = new safeHistory();
+    var heartB = new heartBleed();
+    heartB.ctor.then(function() {
+        heartB.parse();
+        inst.getHistory(1000).then(function(data) {
+            // Filter data to eliminate duplicate hostnames
+            var hostnames = {};
+
+            // console.log(data);
+            $.each(data, function(key, value) {
+                var uri = new URI(value.url);
+                var hostname = uri.hostname().toLowerCase();
+                if (uri.protocol() != "http" && uri.protocol() != "https") {
+                    return; // Ignore any urls that are not http or https, can't deal with that really.
+                }
+
+                if (!hostnames[hostname]) hostnames[hostname] = [];
+                value.printableDate = new Date(value.lastVisitTime).toString();
+                hostnames[hostname].push(value);
+            });
+            // console.log(hostnames);
+
+            var sites = Object.keys(hostnames);
+
+            getWOTRate(sites).then(function(results) {
+                
+            });
+
+            getGoogleSafeBrowsingRate(sites).then(function(results) {
+                
+            });
+
+        });
+    });
+}
