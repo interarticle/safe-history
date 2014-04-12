7c9c36e0e7625e219e0bb1cfef4b64ced8414459
Oliver <wzy@umich.edu>
2014-04-11 22:22:55 -0400

    hb

--- main.js
+++ main.js
@@ -8,20 +8,43 @@ function safeHistory() {
 	}
 }
 
+function heartBleed () {
+	var currentUrl;
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+	    currentUrl = tabs[0].url;
+	});
+
+	var parse = function (data) {
+		var urls = data.split('\n');
+		// var currentUrl = tabs[0].url;
+
+		console.log(urls[0]);
+		var currentDomain = currentUrl.split('/')[2];
+		console.log(currentDomain);
+
+		for ( var i = 0; i < urls.length; i++ )
+			if (currentDomain.indexOf(urls[i]) != -1)
+				console.log("This website was vulnerable, you might wanna change your password!")
+
+	}
+
+	this.main = function () {
+		$.get("https://raw.githubusercontent.com/interarticle/safe-history/master/heartbleed.txt", function(data) {
+			// console.log(data);
+			parse(data);
+		})
+	}
+}
+
 (function($, undefined) {
 	var inst = new safeHistory();
+	var heartB = new heartBleed();
 	$(function() {
 		$("#actionbtn").click(function() {
 			inst.getHistory();
-			$.get("https://raw.githubusercontent.com/interarticle/safe-history/master/README.md", function(data) {
-				console.log(data);
-			})
+			heartB.main();
 		});
 	});
 })(jQuery);
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
-   $('#warning').text(tabs[0].url);
 
-});
